fd5e977dfd56b1282ade4ea25ab5cbe7f040781f
waseem-ceative92 <waseem-ceative.co.uk>
2024-05-05 00:00:23 +0500

    adding skills images

--- app/sections/home/skills/technical-skills/index.tsx
+++ app/sections/home/skills/technical-skills/index.tsx
@@ -4,19 +4,25 @@ import Slider from "@mui/material/Slider";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { technicalSkillsData } from "./technical-skills-data";
-import { Grid } from "@mui/material";
+import {
+  technicalSkillsCardData,
+  technicalSkillsData,
+} from "./technical-skills-data";
+import { Grid, Theme, alpha } from "@mui/material";
+import CustomCard from "@/app/components/custom-card";
+import Image from "next/image";
 
-const PrettoSlider = styled(Slider)({
+const DynamicSlider = styled(Slider)({
   color: "#52af77",
   height: 8,
-  "& .MuiSlider-track": {
+  "& .MuiSlider-track": (theme: Theme) => ({
     border: "none",
-  },
-  "& .MuiSlider-thumb": {
+    backgroundColor: theme.palette.primary.main,
+  }),
+  "& .MuiSlider-thumb": (theme: Theme) => ({
     height: 24,
     width: 24,
-    backgroundColor: "#fff",
+    backgroundColor: theme.palette.primary.main,
     border: "2px solid currentColor",
     "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
       boxShadow: "inherit",
@@ -24,7 +30,7 @@ const PrettoSlider = styled(Slider)({
     "&::before": {
       display: "none",
     },
-  },
+  }),
   "& .MuiSlider-valueLabel": {
     lineHeight: 1.2,
     fontSize: 12,
@@ -50,22 +56,52 @@ export default function TechnicalSkills() {
   return (
     <Box flexGrow={1}>
       <Grid container spacing={2}>
-        {technicalSkillsData?.map((item) => {
-          return (
-            <Grid item xs={12} xl={6} key={item?.id}>
-              <Typography gutterBottom>{item.title}</Typography>
-              <PrettoSlider
-                valueLabelDisplay="on"
-                aria-label="pretto slider"
-                defaultValue={item?.value}
-                disabled
-              />
-            </Grid>
-          );
-        })}
-
-        <Grid item xs={12} xl={6}></Grid>
-        adjflsjfdljsj
+        <Grid item xs={12} xl={6} lg={6} md={6} sm={12}>
+          {technicalSkillsData?.map((item) => {
+            return (
+              <Box key={item?.id} sx={{ maxWidth: 500 }}>
+                <Typography gutterBottom>{item?.title}</Typography>
+                <DynamicSlider
+                  // valueLabelDisplay="on"
+                  aria-label="pretto slider"
+                  defaultValue={parseInt(item?.value)}
+                  disabled
+                  sx={{
+                    "& .MuiSlider-thumb": (theme: Theme) => ({
+                      backgroundColor: theme.palette.primary.main,
+                    }),
+                    "& .MuiSlider-track": (theme: Theme) => ({
+                      backgroundColor: alpha(theme.palette.primary.light, 0.5),
+                    }),
+                  }}
+                />
+              </Box>
+            );
+          })}
+        </Grid>
+        <Grid item xs={12} xl={6} lg={6} md={6} sm={12}>
+          <Grid container spacing={2}>
+          {technicalSkillsCardData?.map((item) => {
+            return (
+              <Grid item xs={6} xl={3} lg={3} md={3} sm={6}>
+              <CustomCard key={item?.id} sxProps={{width:"120px", height:"120px" ,cursor:"pointer"}}>
+                <Box display="flex" justifyContent="space-between" alignItems='center' flexDirection='column' gap={1}>
+                  <Box mt={2} sx={{borderRadius:"50%", border:'none' ,width:"60px", height:"60px" }}>
+                    <Image
+                      src={item?.imgIcon}
+                      alt="skill icon"
+                      priority
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                  </Box>
+                  <Typography variant="subtitle1">{item?.title}</Typography>
+                </Box>
+              </CustomCard>
+              </Grid>
+            );
+          })}
+           </Grid>
+        </Grid>
       </Grid>
     </Box>
   );
--- app/sections/home/skills/technical-skills/technical-skills-data.ts
+++ app/sections/home/skills/technical-skills/technical-skills-data.ts
@@ -1,15 +1,26 @@
+import htmlIcon from '../../../../assets/skills-icons/html.svg'
+import cssIcon from '../../../../assets/skills-icons/css.svg'
+import reactIcon from '../../../../assets/skills-icons/react.svg'
+import nextIcon from '../../../../assets/skills-icons/nextJS.svg'
+import javascriptIcon from '../../../../assets/skills-icons/javascript.svg'
+import typescriptIcon from '../../../../assets/skills-icons/typescript.svg'
+import cPlusIcon from '../../../../assets/skills-icons/cplusIcon.svg'
+import oopIcon from '../../../../assets/skills-icons/oopIocn.png'
+import apiIcon from '../../../../assets/skills-icons/apiIcon.png'
+import reduxIcon from '../../../../assets/skills-icons/redux-icon.svg'
 
 export interface ISliderData {
   id: string;
   title: string;
   value:any;
 }
+ 
 
 export const technicalSkillsData: ISliderData[] = [
   {
     id: "1",
     title:"Frontend UI/UX",
-    value:"99%",
+    value:"95%",
   },
   {
     id: "2",
@@ -19,31 +30,95 @@ export const technicalSkillsData: ISliderData[] = [
   {
     id: "3",
     title:"Analytic",
-    value:"98%",
+    value:"90%",
   },
   {
     id: "4",
     title:"Unit Testing",
-    value:"98%",
+    value:"93%",
   },
   {
     id: "5",
     title:"Debugging",
-    value:"98%",
+    value:"90%",
   },
   {
     id: "6",
     title:"Cross Browser Compatibility",
-    value:"98%",
+    value:"92%",
+  },
+  // {
+  //   id: "7",
+  //   title:"ReactJs/Redux/Redux Thunk/RTK Query/RESTful API",
+  //   value:"88%",
+  // },
+  // {
+  //   id: "8",
+  //   title:"NextJs",
+  //   value:"88%",
+  // },
+]
+
+export interface ITechnicalSkillCardData {
+  id: string;
+  title: string;
+  imgIcon?:any;
+}
+export const technicalSkillsCardData: ITechnicalSkillCardData[]=[
+  {
+    id:"1",
+    title:"Next JS",
+    imgIcon:nextIcon,
   },
   {
-    id: "7",
-    title:"ReactJs/Redux/Redux Thunk/RTK Query/RESTful API",
-    value:"88%",
+    id:"2",
+    title:"React JS",
+    imgIcon:reactIcon,
   },
   {
-    id: "8",
-    title:"NextJs",
-    value:"88%",
+    id:"3",
+    title:"JavaScript",
+    imgIcon:javascriptIcon,
   },
+  
+  {
+    id:"4",
+    title:"TypeScript",
+    imgIcon:typescriptIcon,
+  },
+  {
+    id:"5",
+    title:"Redux",
+    imgIcon:reduxIcon,
+  },
+  {
+  id:"6",
+  title:"RTK Query",
+  imgIcon:reduxIcon,
+},
+{
+  id:"7",
+  title:"RESTful API",
+  imgIcon:apiIcon,
+},
+{
+  id:"8",
+  title:"HTML",
+  imgIcon:htmlIcon,
+},
+{
+  id:"9",
+  title:"CSS",
+  imgIcon:cssIcon,
+},
+{
+  id:"10",
+  title:"C/C++",
+  imgIcon:cPlusIcon,
+},
+{
+  id:"11",
+  title:"OOP",
+  imgIcon:oopIcon,
+},
 ]
